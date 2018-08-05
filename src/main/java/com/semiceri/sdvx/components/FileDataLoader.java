100130cfee2ca59e88ac0edb37f5271210cd46b4
Kenneth Till <kenneth.g.till@gmail.com>
2018-08-04 20:11:42 -0700

    adding in jackson for use with file based loading

--- src/main/java/com/semiceri/sdvx/components/FileDataLoader.java
+++ src/main/java/com/semiceri/sdvx/components/FileDataLoader.java
@@ -1,8 +1,13 @@
 package com.semiceri.sdvx.components;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.semiceri.sdvx.model.Song;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +17,14 @@ public class FileDataLoader implements DataLoader {
     public List<Song> LoadData() {
         List<Song> loadedList = new ArrayList<>();
 
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            InputStream in = this.getClass().getClassLoader().getResourceAsStream("songs.json");
+            loadedList = mapper.readValue(in, new TypeReference<List<Song>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         return loadedList;
--- src/main/java/com/semiceri/sdvx/model/Song.java
+++ src/main/java/com/semiceri/sdvx/model/Song.java
@@ -1,14 +1,17 @@
 package com.semiceri.sdvx.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 public class Song {
     private String name;
     private List<Integer> difficulties;
+
+    @java.beans.ConstructorProperties({"name", "difficulties"})
+    public Song(String name, List<Integer> difficulties) {
+        this.name = name;
+        this.difficulties = difficulties;
+    }
 }
