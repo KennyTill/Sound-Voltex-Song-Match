5fab6e1cc77cd22aec04dd322aa1bbca99322e27
Kenneth Till <kenneth.g.till@gmail.com>
2018-08-06 15:44:09 -0700

    cleanup of unused functions in main.js

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,36 +1,4 @@
-//
-//
-// function submitFormToEndpoint(){
-//     console.log("inside submit function");
-//     //Call submit action on form
-//     $("#form-search").submit(function (e){
-//
-//         console.log("inside submit form action");
-//         var form = $("#form-search");
-//         console.log(form);
-//         var url = form.attr("action");
-//
-//         //ajax the form out to our matches endpoint
-//         $.ajax({
-//             type: "POST",
-//             url: url,
-//             data: form.serialize(),
-//             success:function (data){
-//                 console.log(data);
-//             },
-//             error: function (data){
-//                 console.log(data)
-//             }
-//         });
-//         //stop standard post submit
-//         e.preventDefault();
-//     });
-// }
-//
-// $(document).ready(function(){
-//     submitFormToEndpoint();
-// });
-
+//Search Functionality
 $("#btn-search").click(function(e){
     console.log('things!');
     e.preventDefault();
@@ -46,27 +14,3 @@ $("#btn-search").click(function(e){
         }
     });
 });
-// $("#form-search").on("submit", function(e){
-//     console.log("inside submit form action");
-//
-//     //stop standard post submit
-//     e.preventDefault();
-//
-//     var form = $(this);
-//     console.log(form);
-//     var url = form.attr("action");
-//
-//     //ajax the form out to our matches endpoint
-//     $.ajax({
-//         type: "POST",
-//         url: url,
-//         data: form.serialize(),
-//         success:function (data){
-//             console.log(data);
-//         },
-//         error: function (data){
-//             console.log(data)
-//         }
-//     });
-//
-// });
